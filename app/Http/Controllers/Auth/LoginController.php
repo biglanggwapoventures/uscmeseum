f03a8cf553f417fdc46bcb309ab524aa9b63590a
Adrian Natabio <adrian@lanexus.com>
2019-04-16 01:00:13 +0800

    added more condition to autj

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 
 class LoginController extends Controller
 {
@@ -40,16 +44,44 @@ class LoginController extends Controller
     /**
      * Validate the user login request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return void
      *
      * @throws \Illuminate\Validation\ValidationException
      */
     protected function validateLogin(Request $request)
     {
-        $request->validate([
-            $this->username() => 'required|string',
-            'password' => 'required|string',
+        $validator = \Validator:: make($request->all(), [
+            $this->username() => [
+                'required',
+                'string',
+                Rule::exists('users')
+            ],
+            'password'        => 'required|string',
+        ], [
+            "{$this->username()}.exists" => "The given account does not exists or has not been activated yet."
         ]);
+
+        $validator->after(function ($validator) use ($request) {
+            if ($validator->errors()->count()) {
+                return;
+            }
+            $user = User::query()
+                        ->where($this->username(), $request->input($this->username()))
+                        ->first();
+
+            if ( ! $user->enabled_at && $user->isRole('standard')) {
+                $validator->errors()->add($this->username(), 'Account is not yet activated.');
+                return;
+            }
+
+            if ( ! Hash::check($request->input('password'), $user->password)) {
+                $validator->errors()->add('password', 'Incorrect password');
+            }
+
+
+        });
+
+        $validator->validate();
     }
 }
